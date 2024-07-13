64661960d9cc8c2eef744479d4784e3c1331252b
Ely Dev <57238178+ElyDeveloper@users.noreply.github.com>
2024-07-13 09:43:20 -0600

    Integracion de cambio modo oscuro/claro

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,19 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject } from "@angular/core";
+import { Platform } from "@ionic/angular";
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"],
 })
 export class AppComponent {
+  darkMode: boolean = true;
+
   public appPages = [
-    { title: 'Inbox', url: '/home/inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/home/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/home/favorites', icon: 'heart' },
-    { title: 'Archived', url: '/home/archived', icon: 'archive' },
-    { title: 'Trash', url: '/home/trash', icon: 'trash' },
-    { title: 'Spam', url: '/home/spam', icon: 'warning' },
+    { title: "Inbox", url: "/home/inbox", icon: "mail" },
+    { title: "Outbox", url: "/home/outbox", icon: "paper-plane" },
+    { title: "Favorites", url: "/home/favorites", icon: "heart" },
+    { title: "Archived", url: "/home/archived", icon: "archive" },
+    { title: "Trash", url: "/home/trash", icon: "trash" },
+    { title: "Spam", url: "/home/spam", icon: "warning" },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() { }
-  
+  public labels = ["Family", "Friends", "Notes", "Work", "Travel", "Reminders"];
+
+  constructor(private platform: Platform) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.checkDarkMode();
+    });
+  }
+
+  checkDarkMode() {
+    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)");
+    this.darkMode = prefersDark.matches;
+    if (prefersDark.matches) {
+      document.body.classList.toggle("dark");
+    }
+  }
+
+  cambioApariencia() {
+    this.darkMode = !this.darkMode;
+    document.body.classList.toggle("dark");
+  }
 }
