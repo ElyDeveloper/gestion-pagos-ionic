190e01685a2a8aa9e2ab2bd38a41ae21fcfae42a
Ely Dev <57238178+ElyDeveloper@users.noreply.github.com>
2024-07-22 17:05:50 -0600

    Refactorizar còdigo para la nueva forma

--- frontend/src/app/pages/layout.page.ts
+++ frontend/src/app/pages/layout.page.ts
@@ -1,66 +1,14 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
-import { filter } from 'rxjs';
+import { Component, inject, OnInit } from "@angular/core";
+import { ActivatedRoute, NavigationEnd, Router } from "@angular/router";
+import { filter } from "rxjs";
 
 @Component({
-  selector: 'app-layout',
-  templateUrl: './layout.page.html',
-  styleUrls: ['./layout.page.scss'],
+  selector: "app-layout",
+  templateUrl: "./layout.page.html",
+  styleUrls: ["./layout.page.scss"],
 })
 export class LayoutPage implements OnInit {
-  selectionProfile: any;
+  constructor() {}
 
-  private activatedRoute = inject(ActivatedRoute);
-  private router = inject(Router);
-  user = {
-    name: 'John Doe',
-    email: 'john@gmail.com',
-  }
-
-  segments: any[] = [];
-
-  
-  constructor() { }
-  
-  ionViewWillEnter() {
-    this.selectionProfile = '';
-  }
-
-  ngOnInit() {
-     // Suscribirse a los eventos del router para capturar cambios en la URL
-  this.router.events
-  .pipe(
-    filter((event:any) => event instanceof NavigationEnd) // Filtrar solo los eventos de finalización de navegación
-  )
-  .subscribe(() => {
-    const url = this.router.url; // Obtener la URL actual
-    this.segments = url.split('/'); // Dividir la URL en segmentos
-    //Eliminar primeros dos elementos del array
-    this.segments = this.segments.slice(2);
-
-    //Si el segmento es igual a home eliminarlo
-    if (this.segments[0] === 'home') {
-      this.segments = [];
-    }
-    
-    console.log('Segments: ', this.segments); // Loggear los segmentos
-  });
-  }
-
-  logout() {
-    console.log('logout');
-    this.router.navigate(['/login']);
-  }
-
-  getSelection() {
-    switch (this.selectionProfile) {
-      case 'logout':
-        this.logout();
-        break;
-      default:
-        break
-    }
-    
-  }
-
+  ngOnInit(): void {}
 }
--- frontend/src/app/shared/components/breadcrumb/breadcrumb.component.ts
+++ frontend/src/app/shared/components/breadcrumb/breadcrumb.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, OnInit } from "@angular/core";
 import { NavigationEnd, Router } from "@angular/router";
-import { filter } from "rxjs";
+import { filter, Subscription } from "rxjs";
 
 @Component({
   selector: "app-breadcrumb",
@@ -12,29 +12,35 @@ export class BreadcrumbComponent implements OnInit {
 
   segments: any[] = [];
 
+  subscriptions: Subscription[] = [];
+
   constructor() {}
 
   ngOnInit() {
     // Suscribirse a los eventos del router para capturar cambios en la URL
-    this.router.events
-      .pipe(
-        filter((event: any) => event instanceof NavigationEnd) // Filtrar solo los eventos de finalización de navegación
-      )
-      .subscribe(() => {
-        const url = this.router.url; // Obtener la URL actual
-        this.segments = url.split("/"); // Dividir la URL en segmentos
-        //Eliminar primeros dos elementos del array
-        this.segments = this.segments.slice(2);
+    this.subscriptions.push(
+      this.router.events
+        .pipe(
+          filter((event: any) => event instanceof NavigationEnd) // Filtrar solo los eventos de finalización de navegación
+        )
+        .subscribe(() => {
+          const url = this.router.url; // Obtener la URL actual
+          this.segments = url.split("/"); // Dividir la URL en segmentos
+          //Eliminar primeros dos elementos del array
+          this.segments = this.segments.slice(2);
 
-        //Si el segmento es igual a home eliminarlo
-        if (this.segments[0] === "home") {
-          this.segments = [];
-        }
+          //Si el segmento es igual a home eliminarlo
+          if (this.segments[0] === "home") {
+            this.segments = [];
+          }
 
-        console.log("Segments: ", this.segments); // Loggear los segmentos
-      });
+          console.log("Segments: ", this.segments); // Loggear los segmentos
+        })
+    );
   }
 
-
-  
+  ngOnDestroy() {
+    // Desuscribirse de todos los eventos al destruir el componente
+    this.subscriptions.forEach((s) => s.unsubscribe());
+  }
 }
