26f0f4ece928e5d4a9546f5a81a31d0de28224a5
ElyDeveloper <gersoneliezer.rr96@gmail.com>
2024-08-08 16:31:11 -0600

    Gestionar set y get de cookie para el vencimiento del codigo de verificacion

--- frontend/src/app/pages/auth/forgot-password/forgot-password.page.ts
+++ frontend/src/app/pages/auth/forgot-password/forgot-password.page.ts
@@ -1,6 +1,9 @@
-import { Component, inject, OnInit } from "@angular/core";
+import { Component, inject, OnInit, ViewChild } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { ActivatedRoute, Router } from "@angular/router";
+import { CookieService } from "ngx-cookie-service";
+import { key } from "src/app/libraries/key.library";
+import { LoaderComponent } from "src/app/shared/components/loader/loader.component";
 import { GlobalService } from "src/app/shared/services/global.service";
 
 @Component({
@@ -9,27 +12,38 @@ import { GlobalService } from "src/app/shared/services/global.service";
   styleUrls: ["./forgot-password.page.scss"],
 })
 export class ForgotPasswordPage implements OnInit {
+  @ViewChild(LoaderComponent) loaderComponent!: LoaderComponent;
+
   validateForm!: FormGroup;
   isToastOpen: boolean = false;
   toastMessage: string = "";
+  textLoader: string = "Procesando";
   private _globalService = inject(GlobalService);
+  _cookieService = inject(CookieService);
+
   private router = inject(Router);
   constructor(private fb: FormBuilder) {
     this.validateForm = this.fb.group({
-      identificator: ["", [Validators.required]],
+      identificator: ["", [Validators.required, Validators.email]],
     });
   }
 
-  ngOnInit() {
-    
-  }
+  ngOnInit() {}
 
   setOpenedToast(value: boolean) {
     this.isToastOpen = value;
   }
 
+  verifyKey(event: any) {
+    if (event.key === "Enter") {
+      this.submitForm();
+    }
+  }
+
   submitForm(): void {
     if (this.validateForm.valid) {
+      this.textLoader = "Generando código, por favor espere";
+      this.loaderComponent.show();
       this._globalService
         .Post("send-email", {
           identificator: this.validateForm.value.identificator,
@@ -38,15 +52,23 @@ export class ForgotPasswordPage implements OnInit {
           option: 1,
         })
         .subscribe((result: any) => {
+          console.log(result);
           if (result.error) {
             //Mostrar toast
             this.toastMessage = result.error;
             this.isToastOpen = true;
+            this.loaderComponent.hide();
           } else {
-            //Mostrar toast
-            this.toastMessage =
-              "Se ha enviado un correo a su dirección de correo electrónico";
+            //Setear la expiracion del token en cookies
+            this._cookieService.set(
+              "expiration-code",
+              result.expiration,
+              key.CODE_VERIFICATION_EXPIRATION_TIME,
+              ""
+            );
+            this.loaderComponent.hide();
             this.router.navigate(["/verify-code"]);
+            // this.router.navigate(["/verify-code"]);
           }
         });
     } else {
@@ -56,6 +78,8 @@ export class ForgotPasswordPage implements OnInit {
           control.updateValueAndValidity({ onlySelf: true });
         }
       });
+      this.toastMessage = "Por favor, coloque un correo válido.";
+      this.isToastOpen = true;
     }
   }
 }
