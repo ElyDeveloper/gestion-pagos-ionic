abc949d4e446fd0934f5e69ccae79c87df7f06eb
Ely Dev <57238178+ElyDeveloper@users.noreply.github.com>
2024-09-01 14:12:04 -0600

    Configurar Nuevo nombre a la App

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   apiURL: "http://localhost:3004/",
   company: "Sociedad Financiamiento de Crédito Efectivo (FICRE) S. de R. L.",
-  percentage: 3
+  percentage: 3,
+  appName: "LAP - Loan Administration Platform",
 };
