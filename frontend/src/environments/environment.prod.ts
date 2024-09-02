dc5f54d71b2cf6dd01b841ade87313f2c5812118
ElyDeveloper <gersoneliezer.rr96@gmail.com>
2024-09-02 09:01:49 -0600

    Configuracion de Prd

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURL: "http://192.168.0.13:3001/",
+  apiURL: "http://192.168.100.5:3001/",
   company: "Sociedad Financiamiento de Crédito Efectivo (FICRE) S. de R. L.",
   percentage: 3,
   appName: "LAP - Loan Administration Platform",
