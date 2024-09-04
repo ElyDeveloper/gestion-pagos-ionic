3479c815e2d0c89afb89754bc5379e4f2e95d2c1
Ely Dev <57238178+ElyDeveloper@users.noreply.github.com>
2024-09-03 22:18:06 -0600

    Configurar para PRD

--- backend/src/datasources/gestion-edbo.datasource.ts
+++ backend/src/datasources/gestion-edbo.datasource.ts
@@ -2,15 +2,30 @@ import {inject, lifeCycleObserver} from '@loopback/core';
 import {juggler} from '@loopback/repository';
 require('dotenv').config();
 
+// const config = {
+//   name: 'GestionEDBO',
+//   connector: 'mssql',
+//   url: process.env.URL_DB,
+//   host: process.env.HOST_DB,
+//   port: process.env.PORT_DB,
+//   user: process.env.USER_DB,
+//   password: process.env.PASSWD_DB,
+//   database: process.env.DB_DB,
+//   requestTimeout: 300000,
+//   options: {
+//     enableArithAbort: true,
+//   },
+// };
+
 const config = {
   name: 'GestionEDBO',
   connector: 'mssql',
-  url: process.env.URL_DB,
-  host: process.env.HOST_DB,
-  port: process.env.PORT_DB,
-  user: process.env.USER_DB,
-  password: process.env.PASSWD_DB,
-  database: process.env.DB_DB,
+  url: 'mssql://FICRESYSTEM:asd.456@DESKTOP-4JLM67I/Cobros',
+  host: 'DESKTOP-4JLM67I',
+  port:1433,
+  user: 'FICRESYSTEM',
+  password: 'asd.456',
+  database: 'Cobros',
   requestTimeout: 300000,
   options: {
     enableArithAbort: true,
