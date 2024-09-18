3f301e4068d1451d4abce3df3e375ce3c9da7cd1
Ely Dev <57238178+ElyDeveloper@users.noreply.github.com>
2024-09-17 21:51:34 -0600

    Mejorar Elementos a mostrar en reportes

--- frontend/src/app/pages/reportes/report-cartera-asesores/report-cartera-asesores.component.ts
+++ frontend/src/app/pages/reportes/report-cartera-asesores/report-cartera-asesores.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, Input, OnInit } from "@angular/core";
 import { catchError, firstValueFrom, tap } from "rxjs";
 import { Cartera } from "src/app/shared/interfaces/report-cartera";
+import { EncabezadoCarteraAsesor } from "src/app/shared/interfaces/reporte-cartera-asesor";
 import { GlobalService } from "src/app/shared/services/global.service";
 
 @Component({
@@ -13,6 +14,19 @@ export class ReportCarteraAsesoresComponent implements OnInit {
   @Input() selectedAsesor: any = null;
 
   carteraAsesor: Cartera[] = [];
+  dateNow: Date = new Date();
+
+  encabezado: EncabezadoCarteraAsesor = {
+    montoOtorgado: 0,
+    saldoCapitalActual: 0,
+    saldoActual: 0,
+    valorCuota: 0,
+    tasaPromedio: 0,
+    valorTotalPagar: 0,
+    valorCuotasAtrasada: 0,
+    mora: 0,
+    totalConMora: 0,
+  };
 
   private _globalService = inject(GlobalService);
   constructor() {}
@@ -21,7 +35,14 @@ export class ReportCarteraAsesoresComponent implements OnInit {
     this.getCarteraAsesor(this.selectedAsesor);
   }
 
-  async getCarteraAsesor(asesor:any) {
+  subtractHours(date: Date, hours: number): Date {
+    const newDate = new Date(date);
+    newDate.setHours(newDate.getHours() - hours);
+    return newDate;
+  }
+
+
+  async getCarteraAsesor(asesor: any) {
     // prestamos/reporte-cartera-asesor?idUsuario=4
 
     if (!asesor) return;
@@ -39,6 +60,40 @@ export class ReportCarteraAsesoresComponent implements OnInit {
           tap((cartera: any) => {
             console.log("Cartera asesor:", cartera);
             this.carteraAsesor = cartera;
+
+            const initialEncabezado = {
+              montoOtorgado: 0,
+              saldoCapitalActual: 0,
+              saldoActual: 0,
+              valorCuota: 0,
+              tasaPromedio: 0,
+              valorTotalPagar: 0,
+              valorCuotasAtrasada: 0,
+              mora: 0,
+              totalConMora: 0,
+            };
+
+            this.encabezado = cartera.reduce(
+              (acc: any, item: any) => ({
+                montoOtorgado: acc.montoOtorgado + item.montoOtorgado,
+                saldoCapitalActual:
+                  acc.saldoCapitalActual + item.saldoCapitalActual,
+                saldoActual: acc.saldoActual + item.saldoActual,
+                valorCuota: acc.valorCuota + item.valorCuota,
+                tasaPromedio: acc.tasaPromedio + item.tasa,
+                valorTotalPagar: acc.valorTotalPagar + item.valorTotal,
+                valorCuotasAtrasada:
+                  acc.valorCuotasAtrasada + item.valorCuotasAtrasadas,
+                mora: acc.mora + item.mora,
+                totalConMora: 0, // We'll calculate this after the reduce
+              }),
+              initialEncabezado
+            );
+
+            // Calculate average rate and total with late fees
+            this.encabezado.tasaPromedio /= cartera.length;
+            this.encabezado.totalConMora =
+              this.encabezado.montoOtorgado + this.encabezado.mora;
           }),
           catchError((error) => {
             console.error("Error fetching prestamo:", error);
--- frontend/src/app/pages/reportes/report-estado-cuenta/report-estado-cuenta.component.ts
+++ frontend/src/app/pages/reportes/report-estado-cuenta/report-estado-cuenta.component.ts
@@ -18,25 +18,6 @@ export class ReportEstadoCuentaComponent implements OnInit {
   elements: any[] = [];
   saldosVigentes: any[] = [];
 
-  // export interface EncabezadoEstadoCuenta {
-  //   nroPrestamo: number | string;
-  //   codClientes: string;
-  //   estadoPtmo: boolean | string;
-  //   mtoPrestamo: number;
-  //   saldoPtmo: number;
-  //   asesor: number | string;
-  //   nombreAsesor: string;
-  //   tMora: string;
-  //   fDesembolso: string;
-  //   producto: string;
-  //   cuota: number;
-  //   plazo: number | string;
-  //   Periodo: string;
-  //   direccion: string;
-  //   telefono: string;
-  //   mora: number;
-  //   totalSTotales: number;
-  // }
   encabezado: EncabezadoEstadoCuenta = {
     nroPrestamo: "N/A",
     codClientes: "N/A",
